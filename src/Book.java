d8e154604ed1d2842dc65b50d8e283c68ff89c4b
LucaDalRi <euro2xd@gmail.com>
2023-06-07 17:50:31 +0200

    eccezzioni costruttore inserite

--- src/Book.java
+++ src/Book.java
@@ -6,41 +6,54 @@ public class Book {
     private String editore;
 
     public Book(String titolo, int nPagine, String autore, String editore) {
-        this.titolo = titolo;
-        this.nPagine = nPagine;
-        this.autore = autore;
-        this.editore = editore;
+        if (!titolo.isBlank() && !titolo.isEmpty()) {
+            this.titolo = titolo;
+        } else {
+            throw new RuntimeException("Dati errari");
+        }
+        if (nPagine > 0) {
+            this.nPagine = nPagine;
+        } else {
+            throw new RuntimeException("Dati errati");
+        }
+        if (!autore.isBlank() && !autore.isEmpty()) {
+            this.autore = autore;
+        } else {
+            throw new RuntimeException("Dati errati");
+        }
+        if (!editore.isBlank() && !editore.isEmpty()) {
+            this.editore = editore;
+        } else {
+            throw new RuntimeException("Dati errati");
+        }
     }
 
     public String getTitolo() {
         return titolo;
     }
-
-    public void setTitolo(String titolo) {
-        this.titolo = titolo;
-    }
-
     public int getnPagine() {
         return nPagine;
     }
-
-    public void setnPagine(int nPagine) {
-        this.nPagine = nPagine;
-    }
-
     public String getAutore() {
         return autore;
     }
-
-    public void setAutore(String autore) {
-        this.autore = autore;
-    }
-
     public String getEditore() {
         return editore;
     }
 
-    public void setEditore(String editore) {
+    private void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    private void setnPagine(int nPagine) {
+        this.nPagine = nPagine;
+    }
+
+    private void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    private void setEditore(String editore) {
         this.editore = editore;
     }
 
