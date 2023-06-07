910c21090c07839fc347f9f2be5b4ab8ae5a7862
LucaDalRi <euro2xd@gmail.com>
2023-06-07 17:10:18 +0200

    Push base dei libri nell array no eccezzioni

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,17 +9,34 @@ public class Main {
 
         System.out.println("Quanti libri vuoi inserire?");
 
-        int nLibri = s1.nextInt();
+        int nLibri = Integer.parseInt(s1.nextLine());
 
-
+        Book[] arrLibri = new Book[nLibri];
 
         for (int i = 0; i < nLibri; i++) {
 
+            System.out.println("Inserisci il titolo");
 
+            String titolo = s1.nextLine();
+
+            System.out.println("Inserisci in numeri di pagine");
+
+            int nPagine = Integer.parseInt(s1.nextLine());
+
+            System.out.println("Inserisci l`autore");
+
+            String autore = s1.nextLine();
+
+            System.out.println("Inserisci l`editore");
+
+            String editore = s1.nextLine();
+
+            Book newLibro = new Book(titolo,nPagine,autore,editore);
+
+            arrLibri[i] = newLibro;
 
         }
 
-
-
+        System.out.println(Arrays.toString(arrLibri));
     }
 }
