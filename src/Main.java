5c64a273fbb8ee54016d4ae27cbe009d94259f05
LucaDalRi <euro2xd@gmail.com>
2023-06-07 16:47:15 +0200

    Inizializzazione progetto

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.println("Quanti libri vuoi inserire?");
+
+        int nLibri = s1.nextInt();
+
+
+
+        for (int i = 0; i < nLibri; i++) {
+
+
+
+        }
+
+
+
+    }
+}
